6fbd42361b20c419fb67b566f4d01d091a9fc63d
Sourik Ghosh <sourikghosh31@gmail.com>
2021-01-09 20:05:48 +0530

    changes

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { setAsync, getAsync } from './db'
+import { client, setAsync, getAsync } from './db.js'
 const app = express()
 
 app.get('/', (req, res, next) => {
