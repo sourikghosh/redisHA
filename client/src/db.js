7d52ba68a3c497eb635b519e2882c7c880bdcda3
Sourik Ghosh <sourikghosh31@gmail.com>
2021-01-09 20:05:59 +0530

    modified

--- client/src/db.js
+++ client/src/db.js
@@ -1,9 +1,22 @@
 import { createClient } from 'redis'
 import { promisify } from 'util'
 export const client = createClient({
-
+    port: +process.env.R_PORT || 6379,
+    host: process.env.R_HOST || "localhost",
+    //password: process.env.R_PASSWORD
 })
 
+client.on('connect', () => {
+    console.log('Client connected to redis...')
+})
+
+client.on('ready', () => {
+    console.log('Client connected to redis and ready to use...')
+})
+
+client.on('error', (err) => {
+    console.log(err.message)
+})
 export const getAsync = promisify(client.get).bind(client)
 export const setAsync = promisify(client.set).bind(client)
 
