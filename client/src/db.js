deb24a249a67532109d8f4a68f2512b755f327ea
Sourik Ghosh <sourikghosh31@gmail.com>
2021-01-09 19:19:15 +0530

    modified

--- client/src/db.js
+++
@@ -1,9 +0,0 @@
-import { createClient } from 'redis'
-import { promisify } from 'util'
-export const client = createClient({
-
-})
-
-export const getAsync = promisify(client.get).bind(client)
-export const setAsync = promisify(client.set).bind(client)
-
